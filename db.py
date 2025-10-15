2e8aa8bea5de1572ff301a410749236ae21d8855
GergertLA <ludmila-gergert-13@yandex.com>
2025-10-15 22:12:33 +0500

    Update db.py

--- db.py
+++ db.py
@@ -2,10 +2,11 @@ import psycopg2
 
 def get_db_connection():
     conn = psycopg2.connect(
-        database="NeuroScate",
-        user="postgres",
-        password="13Gergert08",
-        host="localhost",
-        port="5432"
+        database="***",
+        user="***",
+        password="***",
+        host="***",
+        port="***"
     )
+
     return conn
